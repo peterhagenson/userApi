17557a19b53af9c066e2ee88e05a378a7765ad7c
peter hagenson <peterhagenson@peters-Mac-mini.local>
2023-05-29 06:44:21 -0500

    installed cors

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,18 @@ import registerRouter from "./routes/registerRouter"
 import usersRouter from "./routes/usersRouter"
 import connectDB from './db/connect'
 import dotenv from 'dotenv'
+import cors from 'cors'
+
+
 
 
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const port = process.env.PORT
 const url = process.env.MONGO_URL
-
-
-import mongoose from 'mongoose'
 
 app.use('/ping', pingRouter)
 app.use('/login', loginRouter)
