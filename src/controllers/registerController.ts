5bc060b74757f9277667e2fd499972e6a94ebb9d
peter hagenson <peterhagenson@peters-Mac-mini.local>
2023-05-29 11:30:35 -0500

    fixed async issue in register router

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -3,15 +3,18 @@ import User from '../models/user'
 import bcrypt from 'bcryptjs'
 
 const addUser = async (req: Request, res: Response) => {
-    console.log('in register controller', req)
+    //console.log('in register controller', req)
+
+    const { firstName, lastName, email, password } = req.body
+
+
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
     const newUser = new User({    
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: hashedPassword
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPassword
     })
-   
     try {
         const user = await newUser.save()
         if (user) {
--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { Request, Response } from 'express' 
 import {addUser} from '../controllers/registerController'
+
 const router = express.Router();
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
+
   console.log('in registerRouter', req.body)
   try {
-    addUser(req, res)
+    await addUser(req, res)
     res.status(200).send({message: 'user added'})
   } catch (error) {
     res.status(500).json({ msg: 'there was an error' })
